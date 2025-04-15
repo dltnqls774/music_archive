290f17ed81440ad7b98fd05b30389010bb14297c
Subin Lee <dltnqls774@naver.com>
2025-04-15 16:10:52 +0900

    fix: GitHub Pages용 HashRouter 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Home from './pages/Home';
@@ -10,45 +10,27 @@ import MyCart from './pages/MyCart';
 import NotFound from './pages/NotFound';
 import ProtectedRoute from './pages/ProtectedRoute';
 
-const router = createBrowserRouter([
-  {
-    path : '/',
-    element : <App   />,
-    errorElement : <NotFound />,
-    children: [
-      {
-        index: true, 
-        path:'/', 
-        element:<Home />
-      },
-      {
-        path : '/products', 
-        element:<ProductDetail />
-      },
-      {
-        path : '/products/new', 
-        element: (
-          <ProtectedRoute requireAdmin>
-            <NewProduct />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path : '/carts', 
-        element: (
-          <ProtectedRoute>
-            <MyCart />
-          </ProtectedRoute> 
-        ),
-      },
-    ]
-  }
-])
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="products" element={<ProductDetail />} />
+          <Route path="products/new" element={
+            <ProtectedRoute requireAdmin>
+              <NewProduct />
+            </ProtectedRoute>
+          } />
+          <Route path="carts" element={
+            <ProtectedRoute>
+              <MyCart />
+            </ProtectedRoute>
+          } />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
